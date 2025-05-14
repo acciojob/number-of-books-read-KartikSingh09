8c457f12d70562ce7b91c991242916ab4de5aac8
Kartik Singh <167749570+KartikSingh09@users.noreply.github.com>
2025-05-14 08:27:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+function numberOfBooksRead(library) {
+  let count = 0;
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus === true) {
+      count++;
+    }
+  }
 
-// Do not change the code below
+  return count;
+}
